2db08a8dcb15341aa74ccc44b1146dbd5c037745
asad059 <asadshahid930@gmail.com>
2024-02-16 19:42:51 +0000

    commit

---
+++ src/main/java/com/pinnacloud/sample/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.pinnacloud.sample;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+
+	}
+
+	private static Date getLocalCurrentDate() {
+		String m = "PinnaCloud";
+		return new Date();
+
+	}
+
+}
